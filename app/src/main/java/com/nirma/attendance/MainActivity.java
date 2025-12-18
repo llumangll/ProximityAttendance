8d86c9f3cf24c39255d0d9a6a63eef8a0a483c5a
Umang Rabadiya <umangrabadiya18@gmail.com>
2025-12-18 17:01:21 +0530

    Setup: Main Menu with Professor and Student buttons

--- app/src/main/java/com/nirma/attendance/MainActivity.java
+++ app/src/main/java/com/nirma/attendance/MainActivity.java
@@ -1,24 +1,28 @@
 package com.nirma.attendance;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnProf = findViewById(R.id.btnProf);
+        Button btnStudent = findViewById(R.id.btnStudent);
+
+        btnProf.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Professor Mode Selected", Toast.LENGTH_SHORT).show();
+        });
+
+        btnStudent.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Student Mode Selected", Toast.LENGTH_SHORT).show();
         });
     }
 }
